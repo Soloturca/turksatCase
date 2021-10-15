a01f803b951982e29a482c3f1cd34f3a4b6e39c7
baturay.caglasin <baturay.caglasin@vodafone.com>
2021-10-15 10:13:37 +0300

    feature changed 15101

--- src/test/java/com/project/tests/TestRunner.java
+++ src/test/java/com/project/tests/TestRunner.java
@@ -29,9 +29,9 @@ import java.io.IOException;
 
 
 @CucumberOptions(
-        features = "src/test/features/newFeatures/",
+        features = "src/test/features/newFeatures/WorkflowManagement.feature",
         // tags="@JiraScenarioKey1, @JiraScenarioKey2, @Payment",
-        tags = "@Test",
+        tags = "@Finco",
         glue = {"com.project.stepdefs"})
 
 public class TestRunner extends MyTestNGBaseClass {
